d9b235aa4967b3617a881c6c1100ef1dbc40e145
virgilxw <reubenwxw@gmail.com>
2020-12-10 18:21:08 +0800

    autogeneration of temple directory works

--- site/assets/js/temple_info.js
+++ site/assets/js/temple_info.js
@@ -1,13 +1,21 @@
-$.getJSON("./assets/geojson/2_combined.json").then(function (data) {
+$.getJSON("/assets/geojson/2_combined.json").then(function (data) {
 
 	var templeID = $(".templeid").attr("id")
 	var templeinfo = data.filter(x => x["ref_no"] == templeID)
-	console.log(templeinfo[0])
 
-	$("#local_name").append(templeinfo[0]["name_indo"])
+	$("#title").append(templeinfo[0]["name_indo"])
 	$("#hanzi").append(templeinfo[0]["name_hanzi"])
 	$("#pinyin").append(templeinfo[0]["name_pinyin"])
 	$("#neighbourhood").append(templeinfo[0]["neighbourhood"])
-	$("#region").append(templeinfo[0]["region"])
+	$("#region").html(templeinfo[0]["region"])
 
+	var map = L.map('mapcont').setView([0.90925, 108.98463], 12);
+
+	var layer_OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	var marker = L.marker([templeinfo[0]["gps_lat"], templeinfo[0]["gps_lon"]]).addTo(map);
+
+	map.flyTo([templeinfo[0]["gps_lat"], templeinfo[0]["gps_lon"]], 15)
 });
